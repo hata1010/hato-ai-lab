1384b4d3d6b6aca192a41480ca7e655275b31870
HATA <hatacripto@gmail.com>
2026-08-21 15:14:35 -0400

    feat(ganado): register reproduction models with app config

--- apps/ganado/apps.py
+++ apps/ganado/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
 
+
 class GanadoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.ganado'
+
+    def ready(self):
+        # Registra los modelos zootécnicos adicionales sin duplicar
+        # la definición existente de apps.ganado.models.
+        from . import models_reproduccion  # noqa: F401
