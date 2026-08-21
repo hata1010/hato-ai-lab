25736ec75a6523c2cd4920fcb6d87557be6af0b4
HATA <hatacripto@gmail.com>
2026-08-21 18:47:17 -0400

    test: corregir prueba operativa de pesajes

--- apps/ganado/test_pesajes_operativos.py
+++ apps/ganado/test_pesajes_operativos.py
@@ -1,10 +1,8 @@
-from datetime import datetime
 from decimal import Decimal
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-from django.utils import timezone
 
 from apps.core.models import Finca, UsuarioFinca
 
@@ -21,18 +19,8 @@ class PesajesOperativosTests(TestCase):
         UsuarioFinca.objects.create(usuario=self.admin, finca=self.finca, rol="administrador")
         UsuarioFinca.objects.create(usuario=self.operador, finca=self.finca, rol="operador")
         self.especie = Especie.objects.create(nombre="Bovino")
-        self.animal = Animal.objects.create(
-            numero_arete="P-001",
-            sexo="H",
-            especie=self.especie,
-            finca=self.finca,
-        )
-        self.otro_animal = Animal.objects.create(
-            numero_arete="P-002",
-            sexo="M",
-            especie=self.especie,
-            finca=self.otro_finca,
-        )
+        self.animal = Animal.objects.create(numero_arete="P-001", sexo="H", especie=self.especie, finca=self.finca)
+        self.otro_animal = Animal.objects.create(numero_arete="P-002", sexo="M", especie=self.especie, finca=self.otro_finca)
         self.client.force_login(self.admin)
         session = self.client.session
         session["finca_activa_id"] = self.finca.id
@@ -44,45 +32,34 @@ class PesajesOperativosTests(TestCase):
         self.assertContains(response, "Pesajes")
 
     def test_administrador_puede_ver_y_crear(self):
-        response = self.client.post(
-            reverse("ganado:crear_pesaje"),
-            {
-                "animal": self.animal.id,
-                "fecha": "2026-08-21T10:00",
-                "peso_kg": "425.50",
-                "observaciones": "Pesaje de control",
-            },
-        )
+        response = self.client.post(reverse("ganado:crear_pesaje"), {
+            "animal": self.animal.id,
+            "fecha": "2026-08-21T10:00",
+            "peso_kg": "425.50",
+            "observaciones": "Pesaje de control",
+        })
         self.assertRedirects(response, reverse("ganado:detalle_animal", args=[self.animal.id]))
         self.assertTrue(PesajeAnimal.objects.filter(animal=self.animal, peso_kg=Decimal("425.50")).exists())
 
     def test_operador_puede_consultar_pero_no_gestionar(self):
         self.client.force_login(self.operador)
-        response = self.client.get(reverse("ganado:lista_pesajes"))
-        self.assertEqual(response.status_code, 200)
-        response = self.client.get(reverse("ganado:crear_pesaje"))
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(self.client.get(reverse("ganado:lista_pesajes")).status_code, 200)
+        self.assertEqual(self.client.get(reverse("ganado:crear_pesaje")).status_code, 403)
 
     def test_pesaje_no_cruza_de_finca(self):
-        response = self.client.post(
-            reverse("ganado:crear_pesaje"),
-            {
-                "animal": self.otro_animal.id,
-                "fecha": "2026-08-21T10:00",
-                "peso_kg": "500",
-            },
-        )
+        response = self.client.post(reverse("ganado:crear_pesaje"), {
+            "animal": self.otro_animal.id,
+            "fecha": "2026-08-21T10:00",
+            "peso_kg": "500",
+        })
         self.assertEqual(response.status_code, 200)
         self.assertFalse(PesajeAnimal.objects.filter(animal=self.otro_animal).exists())
 
     def test_peso_debe_ser_positivo(self):
-        response = self.client.post(
-            reverse("ganado:crear_pesaje"),
-            {
-                "animal": self.animal.id,
-                "fecha": "2026-08-21T10:00",
-                "peso_kg": "0",
-            },
-        )
+        response = self.client.post(reverse("ganado:crear_pesaje"), {
+            "animal": self.animal.id,
+            "fecha": "2026-08-21T10:00",
+            "peso_kg": "0",
+        })
         self.assertEqual(response.status_code, 200)
         self.assertFalse(PesajeAnimal.objects.filter(animal=self.animal).exists())
