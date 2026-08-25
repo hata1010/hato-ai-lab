f59c5ffc2742e109d61bdc76107920c3f3ac40bc
HATA <hatacripto@gmail.com>
2026-08-25 11:27:12 -0400

    feat: agrega ruta de compras y adquisiciones

--- apps/administrador/urls.py
+++ apps/administrador/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . import views
+from . import views, compras_views
 
 app_name = "administrador"
 
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("indicadores/", views.indicadores, name="indicadores"),
+    path("compras-adquisiciones/", compras_views.compras_adquisiciones, name="compras_adquisiciones"),
 ]
